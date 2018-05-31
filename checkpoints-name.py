1558a137b77f0fcb638c6ddcc110af0f974e0e7a
ALISURE <562282219@qq.com>
2018-05-31 14:39:26 +0800

    Create checkpoints-name.py

---
+++ checkpoints-name.py
@@ -0,0 +1,15 @@
+import os
+from tensorflow.python import pywrap_tensorflow
+
+checkpoint_path = os.path.join("../wave.ckpt-done")
+
+# Read data from checkpoint file
+reader = pywrap_tensorflow.NewCheckpointReader(checkpoint_path)
+var_to_shape_map = reader.get_variable_to_shape_map()
+
+# Print tensor name and values
+for key in var_to_shape_map:
+    print("tensor_name: ", key)
+    # print(reader.get_tensor(key))
+    
+pass
