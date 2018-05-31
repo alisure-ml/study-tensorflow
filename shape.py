5c849c4a66adee57a47b27a64e2a13f261d6be5c
ALISURE <562282219@qq.com>
2018-05-31 14:40:30 +0800

    Update shape.py

--- shape.py
+++ shape.py
@@ -1,4 +1,28 @@
 """
+# confuse:shape
+
+### tf:static shape
+> x is a tensor
+- x.get_shape()
+- x.get_shape()[0].value
+- x.get_shape().as_list()
+
+### tf:dynamic shape
+> x is a tensor
+- tf.shape(x)
+
+### tf:set_shape
+> x is a tensor
+- x.set_shape(shape=[2, 2])
+
+### numpy:np.shape
+> x is a numpy list
+- np.shape(x)
+- x.shape
+
+"""
+
+"""
     tf中有两种shape:
         static shape 和 dynamic shape
     1.static shape 用于构建图，由创建这个tensor的op推断得来。
